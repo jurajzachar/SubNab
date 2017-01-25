e92b1a4e05d4beb253f019caad51b690600743f0
jurajzachar <juraj.zachar@gmail.com>
2017-01-25 13:00:54 +0100

    add assembly instruction

--- src/main/scala/com/blueskiron/subnab/Main.scala
+++ src/main/scala/com/blueskiron/subnab/Main.scala
@@ -2,6 +2,10 @@ package com.blueskiron.subnab
 
 import com.blueskiron.subnab.srt.SubNabApp
 
+/**
+ * @author Juraj Zachar
+ *
+ */
 object Main extends App {
   lazy val ui = new SubNabApp
   ui.visible = true
--- src/test/scala/test/com/blueskiron/subnab/srt/ParserSpec.scala
+++ src/test/scala/test/com/blueskiron/subnab/srt/ParserSpec.scala
@@ -2,14 +2,19 @@ package test.com.blueskiron.subnab.srt
 
 import org.scalatest._
 import com.blueskiron.subnab.srt._
+import scala.util.{ Failure, Success }
 
+//TODO
 class SRTParserSpec extends FlatSpec with Matchers {
   "The SRTParser" should "read subtitles file into sections" in new SRTSubtitlesFixture {
-    Parser.parse(buffSource).foreach(println(_))
+    Parser.parse(buffSource) match {
+      case Failure(t) => fail(t)
+      case Success(list) => //profit
+    }
   }
 
   trait SRTSubtitlesFixture {
     import scala.io.Source
-    lazy val buffSource = Source.fromURL(getClass.getResource("/subtitles.srt"))
+    lazy val buffSource = Source.fromURL(getClass.getResource("/Portuguese.srt"))
   }
 }
